0b18d0ecf25a9a3abbcd29034be39cf843c3d0f2
Peter Gani <peter.gani@live.com>
2018-01-21 04:01:15 -0500

    Lab 4 - Initial Commit
    - Initial Commit and some work done.

---
+++ Labs/CST8359_Lab4/src/CST8359_Lab4/Controllers/Home.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CST8359_Lab4.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CST8359_Lab4.Controllers
+{
+    public class Home : Controller
+    {
+        private MovieContext _movieContext;
+
+        public Home(MovieContext context)
+        {
+            _movieContext = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_movieContext.Movies.ToList());
+        }
+
+        public IActionResult CreateMovie(Movie movie)
+        {
+            _movieContext.Movies.Add(movie);
+            _movieContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EditMovie(int id)
+        {
+            var movieToUpdate = (from m in _movieContext.Movies where m.MovieId == id select m).FirstOrDefault();
+
+            return View(movieToUpdate);
+        }
+    }
+}
---
+++ Labs/CST8359_Lab4/src/CST8359_Lab4/Models/MoviesContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CST8359_Lab4.Models
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
