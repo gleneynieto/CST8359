d65ffc721f138da91f84ab9f4f50b064df0469ae
Peter Gani <peter.gani@live.com>
2018-04-10 19:09:45 -0400

    Assignment 1 - Initial Commit

---
+++ Assignments/Assignment1/src/Assignment1/Controllers/Home.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Assignment1.Models;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment1.Controllers
+{
+    public class Home : Controller
+    {
+        private Assignment1DataContext _assignment1DataContext;
+
+        public Home(Assignment1DataContext context)
+        {
+            _assignment1DataContext = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_assignment1DataContext.BlogPosts.ToList());
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult RegisterUser(User user)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _assignment1DataContext.Users.Add(user);
+                _assignment1DataContext.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ValidateLogin(User user)
+        {
+            var userToValidate = (from u in _assignment1DataContext.Users where u.EmailAddress == user.EmailAddress && u.Password == user.Password select u).FirstOrDefault();
+
+            if (userToValidate != null)
+            {
+                //Save User Id in session state
+                HttpContext.Session.SetInt32("_userId", userToValidate.UserId);
+                HttpContext.Session.SetInt32("_roleId", userToValidate.RoleId);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.isLoggedIn = false;
+
+                return RedirectToAction("Login");
+            }
+        }
+
+        public IActionResult DisplayFullBlogPost(int id)
+        {
+            var blogPostToShow = (from b in _assignment1DataContext.BlogPosts where b.BlogPostId == id select b).FirstOrDefault();
+
+            return View(blogPostToShow);
+        }
+
+        public IActionResult PostComment(Comment comment)
+        {
+            _assignment1DataContext.Comments.Add(comment);
+            _assignment1DataContext.SaveChanges();
+
+            return View();
+        }
+
+        public IActionResult AddBlogPost()
+        {
+            return View();
+        }
+
+        public IActionResult CreateBlogPost(BlogPost blogPost)
+        {
+            _assignment1DataContext.BlogPosts.Add(blogPost);
+            _assignment1DataContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Assignments/Assignment1/src/Assignment1/Models/Comment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Comment
+    {
+        public int CommentId
+        {
+            get;
+            set;
+        }
+
+        public int BlogPostId
+        {
+            get;
+            set;
+        }
+
+        public int UserId
+        {
+            get;
+            set;
+        }
+
+        public string Content
+        {
+            get;
+            set;
+        }
+    }
+}
